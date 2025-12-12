8300c7a4b40b9628ad1f51e2fa78050cdeb9a2ad
Berk Dağlı <ynsuexy9483_aehit@students.vizja.pl>
2025-12-12 16:16:44 +0100

    Initial Spring Boot project setup with Gradle

---
+++ src/main/java/com/berkdagli/sowa/SowaApplication.java
@@ -0,0 +1,13 @@
+package com.berkdagli.sowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SowaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SowaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berkdagli/sowa/SowaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkdagli.sowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SowaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
